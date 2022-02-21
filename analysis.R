b8f408218b62bf227cf365641720fcbfd3951f4d
Steven Lu <stelu1@uw.edu>
2022-02-21 15:55:51 -0800

    Relationship between varibles graphics added

--- analysis.R
+++ analysis.R
@@ -1,9 +1,33 @@
 library(tidyverse)
 
+# initial data set
 coviddata <- read.csv("./data/europedata.csv")
 
+# filtered csv file down to omicron (B.1.1.529) and source to GISAID
 omicrondata <- coviddata %>%
-  filter(variant == "B.1.1.529")
+  filter(variant == "B.1.1.529", source == "GISAID")
 
+# filtered csv file down to delta (B.1.617.2) and source to GISAID
 deltadata <- coviddata %>%
   filter(variant == "B.1.617.2", source == "GISAID")
+
+#total new cases table and graph 
+total_cases <- omicrondata %>%
+  group_by(country_code) %>%
+  summarize(totalcases = sum(new_cases)) %>%
+  arrange(desc(totalcases))
+
+total_cases_plot <- ggplot(omicron_cases, aes(x = reorder(country_code, - totalcases), y=totalcases)) +
+  geom_bar(stat="identity") +
+  labs(y = "Total New Cases", x = "Country Code", title = "Total Number of New Cases Vs. Country Code")
+
+# French omicron data
+FR_omicron_cases <- omicrondata %>%
+  filter(country_code == "FR") %>%
+  tail(10)
+
+# French Omicron plot
+FR_omicron_cases_plot <- ggplot(data = FR_omicron_cases, aes(x = year_week, y = percent_variant)) +
+  geom_point(shape = 1) +
+  labs(x = "Year Week", y = "Percentage of Omicron Variant", 
+       title = "French Percentage of Omicron Variant vs Year Week")
