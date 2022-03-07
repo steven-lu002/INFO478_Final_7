497b819748ec5d6ffcc66d4536f383b0cbb32800
Steven Lu <stelu1@uw.edu>
2022-03-06 18:15:07 -0800

    map vis

--- final/analysis.R
+++ final/analysis.R
@@ -7,3 +7,21 @@ countries <- read.csv("data/countries.csv")
 # countries with coordinates
 test <- left_join(coviddata, countries, by='country_code')
 
+# summarize by new cases #omicron
+testmap <- test %>%
+  filter(variant == "B.1.1.529", source == "GISAID") %>%
+  group_by(country_code) %>%
+  select(country_code, longitude, latitude, new_cases) %>%
+  summarize(totalcases = sum(new_cases))
+
+# table with country code, coordinates and total cases
+mapInfo <- left_join(testmap, countries, by='country_code') %>%
+  select(country_code, longitude, latitude, totalcases) %>%
+  drop_na(longitude)
+
+m <-leaflet(mapInfo) %>%
+  addTiles() %>%
+  addCircleMarkers(
+    radius = ~ sqrt(totalcases * .00015),
+    stroke = FALSE, fillOpacity = .5
+  )
